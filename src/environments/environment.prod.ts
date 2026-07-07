891b238ac265d0e504c9c0a98c3b7a62cbb770cb
conceptraedu-byte <conceptra.edu@gmail.com>
2026-07-07 11:54:51 +0530

    prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://myhabackend.onrender.com',
+  apiUrl: 'https://myhabackend-production.up.railway.app',
   razorpayKey: 'rzp_live_RCNi39mnb91nXB'
 };
 
