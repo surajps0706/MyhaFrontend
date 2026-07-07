0184c702f102512c26ce91c31129693621dbf0ba
conceptraedu-byte <conceptra.edu@gmail.com>
2026-07-07 14:27:09 +0530

    chnages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://myhabackend-production.up.railway.app',
+  apiUrl: 'https://api.myhacouture.com',
   razorpayKey: 'rzp_live_RCNi39mnb91nXB'
 };
 
